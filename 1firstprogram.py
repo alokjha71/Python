e1937119317197f05038f1a71c48558b83c5c540
Alok jha <jha61aalok@gmail.com>
2026-07-01 14:55:02 +0545

    python learning from Basic

---
+++ 1firstprogram.py
@@ -0,0 +1,12 @@
+#how to print 
+print("sita ram")
+print("My name is Alok jha.")
+print("I am 22 years old.")
+
+#how to print multiple sentences in using single print
+print("My name is Alok jha.","I am 22 years old.")
+
+# We can also print numbers and also we can perform +,-,* etc in print()
+
+print(23)
+print(23+29)
---
+++ 2Variables.py
@@ -0,0 +1,16 @@
+ # Variables in python
+
+#define variables
+name="Alok"
+age=22
+address="Janakpurdham"
+
+# to print variables
+print(name)
+print(age)
+print(address)
+
+print("my name is :",name)
+print("my age is :",age)
+print("my addreess is :",address)
+
