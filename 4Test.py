b74d48ba3c3792be9f86bea31f8c4563d40315be
Alok jha <jha61aalok@gmail.com>
2026-07-02 14:19:54 +0545

    simple python programs

---
+++ 4Test.py
@@ -0,0 +1,22 @@
+# Print Sum:Program
+a=5
+b=4
+
+sum=a+b
+print(sum)
+
+
+# Print difference:Program
+a=5
+b=4
+
+difference=a-b
+print(difference)
+
+# Print multiply:Program
+
+a=5
+b=30
+
+multiply=a*b
+print(multiply)
