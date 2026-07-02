d0d0f0b0e2b4d8c39dbbdf6555d5cbfa5a2b5ebb
Alok jha <jha61aalok@gmail.com>
2026-07-02 14:21:03 +0545

    Comments in python

---
+++ 5Comments.py
@@ -0,0 +1,13 @@
+ #Comments in python:Are the part of code that does not get executed
+
+#comments : 2 Types
+
+#1) single line:represents using : #
+
+# print("heloo")
+
+#2) Multi line:represents using :"""   """
+
+"""
+print("hello i am multi line comment")
+"""
