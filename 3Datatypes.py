284e290e9520c4a8c2d59fe8e3e75ebabb17fa97
Alok jha <jha61aalok@gmail.com>
2026-07-02 14:18:30 +0545

    Datatypes in python

---
+++ 3Datatypes.py
@@ -0,0 +1,39 @@
+
+# Data types in python:integer,Boolean,string,Float,None
+
+#int: +ve,-ve,0
+
+age=22
+loss=-5
+profit=0
+
+print(type(age))
+print(type(loss))
+print(type(profit))
+
+# Boolean:True,False
+#note:T amd F should be in capital letters
+
+age=23
+old=False
+
+print(type(old))
+
+# String: we write strings in double quotes or single quotes
+
+name="Alok"
+address="janakpurdham"
+
+print(type(name))
+print(type(address))
+
+
+# Float:Decimal values: eg =>1.2,3.9,4.5 etc
+
+price=22.5
+print(type(price))
+
+# None: no value is assigned to variable
+
+a=None
+print(type(a))
