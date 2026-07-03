3e215543bea2661e23aa5af9773f882de67a547c
Alok jha <jha61aalok@gmail.com>
2026-07-03 16:00:28 +0545

    Taking Input in python

---
+++ 8Input.py
@@ -0,0 +1,12 @@
+#Input in python:is used to accept values(using keyboard)from user
+#input():result for input is always string
+
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+address=input("Enter your address: ")
+
+print("Welcome",name)
+print("age :",age)
+print("address :",address)
+
+print(type(age))
