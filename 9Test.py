c1a5a3d7b05085192831c8b62b44e3cc365c717f
Alok jha <jha61aalok@gmail.com>
2026-07-03 16:02:05 +0545

    Practice Questions

---
+++ 9Test.py
@@ -0,0 +1,29 @@
+# Write a program to input two numbers and print them
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+
+sum=num1+num2
+print("sum of two numbers is:",sum)
+
+# Write a program to input side of a square and print its area
+
+side=float(input("Enter the side of square:"))
+print("Area is:",side*side)
+
+# Write a program to input 2 floating point number and print their average
+
+a=float(input("Enter the first number:"))
+b=float(input("Enter the second number:"))
+
+print("Average is:",(a+b)/2)
+
+"""
+Write a program to input two integer numbers (c) and (d)
+and print true if a is greater than b ,if not then false
+ """
+
+c=int(input("Enter the first number:"))
+d=int(input("Enter the Second number:"))
+
+print(c>=d)
