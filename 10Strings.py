dbb1389d7a70a1f483fe80eff29b0c5e65d9e77c
Alok jha <jha61aalok@gmail.com>
2026-07-04 18:54:34 +0545

    Strings in python

---
+++ 10Strings.py
@@ -0,0 +1,23 @@
+# STRINGS:a data type that stores a sequence of chartacters
+
+str1="Alok is my name.\ni am from janakpur."
+str2='i love coding.'
+str3="""my dream is to be computer engineer."""
+
+print(str1)
+print(str2)
+print(str3)
+
+ #NT:we can also add strings using concatenation(+)
+
+print(str1+str2+str3)
+
+# note:we can also find the length of string
+# space,special letters are also counted in length
+
+len1=len(str1)
+print(len1)
+
+
+
+
