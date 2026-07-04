65411e8e06ec6d53d55fa18cc54b9930c206e73a
Alok jha <jha61aalok@gmail.com>
2026-07-04 19:00:34 +0545

    Slicing in python

---
+++ 12Slicing.py
@@ -0,0 +1,8 @@
+#Slicing: Accessing parts of a string
+# str[starting_idx:ending_idx]:ending index is not included
+
+str="Alok jha"
+print(str[1:2])
+print(str[5:])#[5:len(str)]
+print(str[:4])#[0:4]
+
