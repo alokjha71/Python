1a89f40ed5298b28467125315bdd22bc35ff0bb1
Alok jha <jha61aalok@gmail.com>
2026-07-04 18:59:11 +0545

    Idexing in python

---
+++ 11indexing.py
@@ -0,0 +1,13 @@
+"""
+Indexing: index:position
+Index always start with "0"
+Index hgelps to access characters
+"""
+str="Janaki mata"
+ch=str[6]
+print(ch)
+
+
+str="Alok jha"
+ch=str[5]
+print(ch)
