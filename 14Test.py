f4302ca37fe20fd358d620c6c9a8bb8b5d83d216
Alok jha <jha61aalok@gmail.com>
2026-07-05 18:00:56 +0545

    Simple practice programs

---
+++ 14Test.py
@@ -0,0 +1,11 @@
+# Practice questions
+#1: Wap to input user first name and calculate its length
+
+name=input("Enter fisrt name of user:")
+len1=len(name)
+print(len1)
+
+#2:Wap to find occurance of "$" in a string
+
+str="I am from  $ janakpurdham $ I live in ktm now"
+print(str.count("$"))
