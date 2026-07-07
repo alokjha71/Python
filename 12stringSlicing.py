503092366112464eb45bda5f0dff7aca2f892878
Alok jha <jha61aalok@gmail.com>
2026-07-07 17:14:41 +0545

    Slicing in string

---
+++ 12stringSlicing.py
@@ -0,0 +1,12 @@
+#Slicing: Accessing parts of a string
+# str[starting_idx:ending_idx]:ending index is not included
+
+str="Alok jha"
+print(str[1:2])
+print(str[5:])#[5:len(str)]
+print(str[:4])#[0:4]
+
+#Negative slicing:Apple:(-5,-4,-3,-2,-1)
+
+str="Apple"
+print(str[-3:-1])
