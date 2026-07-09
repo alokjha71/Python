122883609df00814737d63b113354f03c67b23fa
Alok jha <jha61aalok@gmail.com>
2026-07-09 18:13:59 +0545

    tuples Methods in python

---
+++ 21tuplesMethods.py
@@ -0,0 +1,11 @@
+# Methods in tuples
+
+#1)tup.index(el):returns index of first occurance
+
+tup=(1,2,3,4,5,2,2)
+print(tup.index(2))#op:1
+
+#2)tup.count(ele):counts total occurance
+
+tup=(1,2,3,4,5,2,2,2,2)
+print(tup.count(2))#op:5
